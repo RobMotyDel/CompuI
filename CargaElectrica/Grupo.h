663f89679d42cbbb8295b3737e23c463fbf4ffe4
Rob Monty Del <71721852+RobMotyDel@users.noreply.github.com>
2022-11-25 19:54:09 -0600

    Add files via upload

---
+++ CargaElectrica/Grupo.h
@@ -0,0 +1,32 @@
+//
+// Created by rober on 25/11/2022.
+//
+
+#ifndef CARGAELECTRICA_GRUPO_H
+#define CARGAELECTRICA_GRUPO_H
+
+#include <string>
+
+using namespace std;
+
+class Grupo
+{
+private:
+    string nombreGrupo;
+    float pkGrupo;
+    int cargaGrupo;
+
+public:
+    Grupo()
+    {
+        nombreGrupo="";
+        pkGrupo=0;
+        cargaGrupo=0;
+    }
+    Grupo(string nom, float pk, int carga);
+
+    float getpk() const;
+    int getcarga() const;
+};
+
+#endif //CARGAELECTRICA_GRUPO_H
